6bc8fc4351200afec17c33c202e3e1d817d760e8
3782291211 <Laklouk98@hotmail.com>
2023-06-21 07:50:04 +0100

    Adding search results component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -5,13 +5,33 @@ import './SearchResults.css';
 class SearchResults extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { offset: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    if (e.target.id === 'next') {
+      if (!this.props.pagination.next) return;
+      this.setState({ offset: this.state.offset + 20 });
+    } else {
+      if (!this.props.pagination.previous || this.state.offset < 20) return;
+      this.setState({ offset: this.state.offset - 20 });
+    }
+
+    this.props.onSearch(this.props.searchTerm, this.state.offset);
   }
 
   render() {
     return (
       <div className="SearchResults">
+        <div className="Pagination">
         <h2>Results</h2>
-        <TrackList tracks={this.props.searchResults} />
+        <div>
+        <button onClick={this.handleClick} id="previous" >Previous</button>
+        <button onClick={this.handleClick} id="next" >Next</button>
+        </div>
+        </div>
+        <TrackList isRemoval={false} onAdd={this.props.onAdd} tracks={this.props.searchResults} />
       </div>
     );
   }
