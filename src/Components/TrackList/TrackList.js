8b4eb304b469038824ed176e9be81978aaa4bd1a
3782291211 <Laklouk98@hotmail.com>
2023-06-21 07:49:33 +0100

    Removing constructor

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,16 +3,11 @@ import './TrackList.css';
 import Track from "../Track/Track";
 
 class TrackList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-   
+  render() { 
     return (
       <div className="TrackList">
-        {this.props.tracks && this.props.tracks.map(track => {
-          return <Track key={track.id} track={track} onAdd={this.props.onAdd} isRemoval={this.props.isRemoval} />
+        {this.props.tracks?.map(track => {
+          return <Track key={track.id} track={track} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />
         })}
       </div>
     );
