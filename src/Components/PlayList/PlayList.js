1cc6314b1d51474cc9c47abc4d5cf034ec995473
3782291211 <Laklouk98@hotmail.com>
2023-06-21 07:50:42 +0100

    Adding playlist component

---
+++ src/Components/PlayList/PlayList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './PlayList.css';
+import TrackList from "../TrackList/TrackList";
+
+class PlayList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    this.props.onNameChange(e.target.value);
+  }
+
+  render () {
+    return (
+      <div className="Playlist">
+        <input onChange={this.handleNameChange} placeholder="Enter playlist name"/>
+        <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
+        <button onClick={this.props.onSave} className="Playlist-save">SAVE TO SPOTIFY</button>
+      </div>
+    );
+  }
+}
+
+export default PlayList;
