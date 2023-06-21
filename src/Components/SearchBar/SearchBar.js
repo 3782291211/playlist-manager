6912af3bfc399224fa211d0f803ee217587e9c85
3782291211 <Laklouk98@hotmail.com>
2023-06-21 07:50:09 +0100

    Adding search bar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,13 +2,31 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component{
+  constructor(props) {
+    super(props);
+    this.search = this.search.bind(this);
+    //this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  search(e) {
+    e.preventDefault();
+    if (!this.props.searchTerm.trim()) {
+      
+    } else {
+      this.props.onSearch(this.props.searchTerm);
+    }
+  }
+
+  // handleTermChange(e) {
+  //   this.setState({ searchTerm: e.target.value });
+  // }
 
   render() {
     return (
-      <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
-        <button className="SearchButton">SEARCH</button>
-      </div>
+      <form onSubmit={this.search} className="SearchBar">
+        <input id="SearchBox" onChange={this.props.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
+        <input type="submit" value="SEARCH" className="SearchButton" />
+      </form>
     );
   }
 }
