02e637a57bdf8f6885ad319355ac85a70bd1e0a1
3782291211 <Laklouk98@hotmail.com>
2023-06-21 07:51:13 +0100

    Updating App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,35 +3,29 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import PlayList from '../PlayList/PlayList';
+import Spotify from '../../util/Spotify';
+import Confirmation from '../Confirmation/Confirmation';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      searchResults: [
-        { name: "go", artist: "john", album: "fire", id: 1},
-        { name: "sunset", artist: "pam", album: "rain", id: 2},
-        { name: "many", artist: "jessy", album: "dance", id: 3},
-        { name: "lift", artist: "neo", album: "anyone", id: 4},
-        { name: "probable", artist: "julian", album: "tomorrow", id: 5},
-        { name: "flow", artist: "hollow", album: "trap", id: 6},
-        { name: "marvel", artist: "bobby", album: "axe", id: 7},
-        { name: "neon", artist: "froll", album: "mime", id: 8},
-        { name: "wrong", artist: "packer", album: "bull", id: 9},
-        { name: "revel", artist: "lights", album: "jelly", id: 10},
-        { name: "dream", artist: "gray", album: "rum", id: 11}
-      ],
+      searchResults: [],
       playlistName: "random songs",
-      playlistTracks: [
-        { name: "marvel", artist: "bobby", album: "fahrbot", id: 1},
-        { name: "neon", artist: "froll", album: "broomstick", id: 2},
-        { name: "wrong", artist: "packer", album: "fruitly", id: 3},
-        { name: "revel", artist: "lights", album: "so-says", id: 4},
-        { name: "dream", artist: "astrobear", album: "sea", id: 5}
-      ]
+      playlistTracks: [],
+      searchTerm: '',
+      pagination: { total: '', previous: '', next: ''}
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ token: Spotify.getAccessToken() });
   }
 
   addTrack(track) {
@@ -48,16 +42,55 @@ class App extends React.Component {
     }); 
   }
 
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
+  async savePlaylist() {
+    const trackURIs = this.state.playlistTracks.map(({ uri }) => uri);
+    const responseStatus = await Spotify.savePlaylist(this.state.playlistName, trackURIs);
+    this.setState({ responseStatus });
+    window.setTimeout(() => this.setState({ responseStatus: null }), 5000);
+    this.setState({ playlistName: 'New playlist', playlistTracks: [] });
+  }
+
+  async search(searchTerm, offset = 0) {
+    const response = await Spotify.search(searchTerm, this.state.token, offset);
+    const searchResults = response?.tracks?.items.map(({album, name, id, artists, uri, preview_url: audioLink}) => {
+      return {
+        album: album.name,
+        name,
+        id,
+        artist: artists[0].name,
+        uri,
+        audioLink
+      }
+    });
+    this.setState({ 
+      searchResults, 
+      pagination: { 
+          total: response?.tracks.total, 
+          previous: response?.tracks.previous, 
+          next: response?.tracks.next 
+        }
+      });
+  }
+
+  handleTermChange(e) {
+    this.setState({ searchTerm: e.target.value });
+  }
+
   render() {
     const { searchResults, playlistName, playlistTracks } = this.state;
     return (
       <div className="relative">
         <h1 className="relative">Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar searchTerm={this.state.searchTerm} handleTermChange={this.handleTermChange} onSearch={this.search} />
+          <Confirmation responseStatus={this.state.responseStatus}/>
           <div className="App-playlist">
-            <SearchResults searchResults={searchResults} onAdd={this.addTrack} />
-            <PlayList playlistName={playlistName} playlistTracks={playlistTracks} />
+            <SearchResults searchResults={searchResults} onAdd={this.addTrack} searchTerm={this.state.searchTerm} onSearch={this.search} pagination={this.state.pagination} />
+            <PlayList playlistName={playlistName} playlistTracks={playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
