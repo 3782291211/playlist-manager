1b1c815162ac15a602c2ad3951f4997d70acc954
3782291211 <Laklouk98@hotmail.com>
2023-06-24 15:48:55 +0100

    Adding prop types and reset button

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,34 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './SearchBar.css';
 
 class SearchBar extends React.Component{
   constructor(props) {
     super(props);
     this.search = this.search.bind(this);
-    //this.handleTermChange = this.handleTermChange.bind(this);
   }
 
   search(e) {
     e.preventDefault();
-    if (!this.props.searchTerm.trim()) {
-      
-    } else {
+    if (this.props.searchTerm.trim()) {
       this.props.onSearch(this.props.searchTerm);
     }
   }
 
-  // handleTermChange(e) {
-  //   this.setState({ searchTerm: e.target.value });
-  // }
-
   render() {
     return (
       <form onSubmit={this.search} className="SearchBar">
-        <input id="SearchBox" onChange={this.props.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
+        <input id="SearchBox" value={this.props.searchTerm} onChange={this.props.handleTermChange} placeholder="Enter A Song, Album, or Artist" />
+        <div className="SearchBarButtons">
         <input type="submit" value="SEARCH" className="SearchButton" />
+        <input type="button" defaultValue="RESET" onClick={this.props.onReset} className="SearchButton" />
+        </div>
       </form>
     );
   }
+}
+
+SearchBar.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+  handleTermChange: PropTypes.func.isRequired,
+  onSearch: PropTypes.func.isRequired,
+  onReset: PropTypes.func.isRequired
 }
 
 export default SearchBar;
