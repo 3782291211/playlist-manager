801c23b47c7a7c12418a2aa9e7e8c513911efec9
3782291211 <Laklouk98@hotmail.com>
2023-06-24 15:48:04 +0100

    Adding conditional rendering for prompt messages

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TrackList from '../TrackList/TrackList';
 import './SearchResults.css';
 
@@ -10,31 +11,48 @@ class SearchResults extends React.Component {
   }
 
   handleClick(e) {
+    let newOffset;
+
     if (e.target.id === 'next') {
       if (!this.props.pagination.next) return;
-      this.setState({ offset: this.state.offset + 20 });
+      newOffset = this.state.offset + 20;
+      //this.setState({ offset: this.state.offset + 20 });
     } else {
       if (!this.props.pagination.previous || this.state.offset < 20) return;
-      this.setState({ offset: this.state.offset - 20 });
+      newOffset = this.state.offset - 20;
+      //this.setState({ offset: this.state.offset - 20 });
     }
-
-    this.props.onSearch(this.props.searchTerm, this.state.offset);
+    
+    this.props.onSearch(this.props.searchTerm, newOffset);
+    this.setState({ offset: newOffset });
   }
 
   render() {
+    const resultsAreEmpty = this.props.searchResults.length === 0;
+
     return (
-      <div className="SearchResults">
+      <div style={resultsAreEmpty ? {height: '7.6rem'} : {}} className="SearchResults">
         <div className="Pagination">
         <h2>Results</h2>
         <div>
-        <button onClick={this.handleClick} id="previous" >Previous</button>
-        <button onClick={this.handleClick} id="next" >Next</button>
+        {!resultsAreEmpty && <><button onClick={this.handleClick} id="previous" >Previous</button>
+        <button onClick={this.handleClick} id="next" >Next</button></>}
         </div>
         </div>
-        <TrackList isRemoval={false} onAdd={this.props.onAdd} tracks={this.props.searchResults} />
+        {resultsAreEmpty && <p className="empty">{this.props.noSearchResults ? 'No results to show.' : 'To view a list of songs, start a new search.'}</p>}
+        <TrackList isRemoval={false} onAdd={this.props.onAdd} tracks={this.props.searchResults}/>
       </div>
     );
   }
+}
+
+SearchResults.propTypes = {
+  searchResults: PropTypes.array.isRequired,
+  onAdd: PropTypes.func.isRequired,
+  searchTerm: PropTypes.string.isRequired,
+  onSearch: PropTypes.func.isRequired,
+  pagination: PropTypes.object.isRequired,
+  noSearchResults: PropTypes.bool.isRequired
 }
 
 export default SearchResults;
